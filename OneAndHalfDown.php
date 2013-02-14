32f47e4f34a4f283bf8ca6c3fad46ab16045a66a
JaakL <jaak.laanpere@gmail.com>
2013-02-14 10:34:05 +0200

    Update OneAndHalfDown.php

--- OneAndHalfDown.php
+++ OneAndHalfDown.php
@@ -2,8 +2,18 @@
    function OneAndHalfDown()
    {
        $dom = new DOMDocument();
-       $dom->load('TulbidJaBonsai_NP.xml');   				// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
+       $dom->load('TulbidJaBonsai_NP.xml'); 					// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
        $xpath = new DOMXPath($dom);
+      
+       $count = $dom->getElementsByTagName('measure')->length;
+       $count = round($count/4);
+       $measures = $xpath->query('//measure');
+		 
+       for ($i = $count; $i < $measures->length; $i++) {
+		    $temp = $measures->item($i); //avoid calling a function twice
+		    $temp->parentNode->removeChild($temp);
+		}
+      
       
      
       /*KEY CHANGING PART*/

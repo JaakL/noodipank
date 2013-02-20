73d3309be65e8b851ca8075b0c9de47c2ab20450
JaakL <jaak.laanpere@gmail.com>
2013-02-20 15:56:00 +0200

    Update SixUp.php

--- SixUp.php
+++ SixUp.php
@@ -1,8 +1,8 @@
 <?php
-   function SixUp()
+   function SixUp($location)
    {
        $dom = new DOMDocument();
-       $dom->load('TulbidJaBonsai_NP.xml');   				// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
+       $dom->load($location); 					// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
        $xpath = new DOMXPath($dom);
       
       
@@ -43,12 +43,6 @@
        		}
    		   
 	   }// END OF TRANSPOSING HARMONY'S BASS PART
-     
-       header("Content-type: text/xml");
-       echo $dom->saveXML();
-       
-       
-   }   
-   $xml = SixUp();
-   
+            $dom->save('sixup.xml');
+   }    
 ?>

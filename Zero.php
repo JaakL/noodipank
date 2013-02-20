98fe1fc6447b8ba1e7861c45014b712eb63e2f67
JaakL <jaak.laanpere@gmail.com>
2013-02-20 15:59:03 +0200

    Update Zero.php

--- Zero.php
+++ Zero.php
@@ -1,21 +1,20 @@
 <?php
-   function Zero()
+   function Zero($location)
    {
        $dom = new DOMDocument();
-       $dom->load('TulbidJaBonsai_NP.xml');   				// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
+       $dom->load($location); 					// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
        $xpath = new DOMXPath($dom);
       
       
-       $count = $dom->getElementsByTagName('measure')->length; 
+       $count = $dom->getElementsByTagName('measure')->length;
        $count = round($count/4);
        $measures = $xpath->query('//measure');
-		  
-        /*Kustutab 3/4 laulust*/  
-        for ($i = $count; $i < $measures->length; $i++) {
-		    $temp = $measures->item($i);
+		 
+       for ($i = $count; $i < $measures->length; $i++) {
+		    $temp = $measures->item($i); //avoid calling a function twice
 		    $temp->parentNode->removeChild($temp);
 		}
+      
             $dom->save('zero.xml');
-   }   
-   $xml = Zero();
+   }
 ?>

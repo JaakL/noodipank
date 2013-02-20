7a76adf168af28c858de4828177a1469174a5707
JaakL <jaak.laanpere@gmail.com>
2013-02-20 15:54:02 +0200

    Update FourDown.php

--- FourDown.php
+++ FourDown.php
@@ -1,8 +1,8 @@
 <?php
-   function FourDown()
+   function FourDown($location)
    {
        $dom = new DOMDocument();
-       $dom->load('TulbidJaBonsai_NP.xml');   				// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
+       $dom->load($location); 					// 	See koht tuleb ära muuta nii, et võtaks Laikre poolt üles laetud ja kliendi poolt valitud laulu XML'i. Hetkel lihtsalt testimiseks üks fail.
        $xpath = new DOMXPath($dom);
       
        $count = $dom->getElementsByTagName('measure')->length;
@@ -767,12 +767,6 @@
 	       }
 		   
 	   }// END OF TRANSPOSING HARMONY'S BASS PART
-     
-       header("Content-type: text/xml");
-       echo $dom->saveXML();
-       
-       
-   }   
-   $xml = FourDown();
-   
+            $dom->save('fourdown.xml');
+   }    
 ?>
